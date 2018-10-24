43c3a8ed724d62ad68ca6388637a739377ca2445
Angie Creed <angie.creed15@gmail.com>
2018-10-24 19:14:01 -0400

    contact about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
-import { NavComponent } from './nav/nav.component';
 import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
-    NavComponent,
-    ProductComponent
+    AboutComponent,
+    ProductComponent,
+    ContactComponent
 
   ],
   imports: [
