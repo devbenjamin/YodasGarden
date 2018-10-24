48232b54eab18446bddd683565ba376a931239e5
Kent B Carlyle II <kentbcarlyle@gmail.com>
2018-10-24 15:54:17 -0400

    add product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
