d4e9029120641a8e4fdc69bd8c92250beb233b78
Kent B Carlyle II <kentbcarlyle@gmail.com>
2018-10-24 19:29:40 -0400

    update modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,15 @@
-
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AboutComponent } from "./about/about.component";
 import { ProductComponent } from "./product/product.component";
-import { ProductService } from "./product.service";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent, AboutComponent, ProductComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [ProductService],
-
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { ProductComponent } from './product/product.component';
-import { ContactComponent } from './contact/contact.component';
-
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    ProductComponent,
-    ContactComponent
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
   providers: [],
-
   bootstrap: [AppComponent]
 })
 export class AppModule {}
