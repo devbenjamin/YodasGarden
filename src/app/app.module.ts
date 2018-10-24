c63b5ef6df2de01412b41916720d1f5581c535b2
Kent B Carlyle II <kentbcarlyle@gmail.com>
2018-10-24 19:14:10 -0400

    add product component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { NavComponent } from './nav/nav.component';
-import { ProductComponent } from './product/product.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AboutComponent } from "./about/about.component";
+import { ProductComponent } from "./product/product.component";
+import { ProductService } from "./product.service";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent
-    NavComponent,
-    ProductComponent
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, AboutComponent, ProductComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
