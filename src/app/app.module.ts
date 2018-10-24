da120c75fd1baac412d3051c859b46a4ef78fd63
Benjamin Donahue <benjamindonahue@Benjamins-MacBook-Pro.local>
2018-10-24 19:27:54 -0400

    mvp navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+// import { ProductComponent } from './product/product.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    // ProductComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
