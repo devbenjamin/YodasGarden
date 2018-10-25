4b305fc979c1db82a276260b17fbfcda280f1a1b
Kent B Carlyle II <kentbcarlyle@gmail.com>
2018-10-25 12:18:16 -0400

    add authorization and misc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { PrivateProductComponent } from "./private-product/private-product.component";
+import { AboutComponent } from "./about/about.component";
+import { ProductComponent } from "./product/product.component";
+import { HomeComponent } from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "privateproduct",
+    component: PrivateProductComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "product",
+    component: ProductComponent
+  },
+  {
+    path: "",
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,29 @@ import { AppComponent } from "./app.component";
 import { AboutComponent } from "./about/about.component";
 import { ProductComponent } from "./product/product.component";
 import { ProductService } from "./product.service";
+import { PrivateProductComponent } from "./private-product/private-product.component";
+import { LoginComponent } from "./login/login.component";
+import { NavComponent } from "./nav/nav.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, AboutComponent, ProductComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ProductComponent,
+    PrivateProductComponent,
+    LoginComponent,
+    NavComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   providers: [ProductService],
   bootstrap: [AppComponent]
 })
